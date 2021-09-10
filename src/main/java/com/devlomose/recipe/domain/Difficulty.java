cb5895fd76831d517900181b68e2bb7628d11972
DevLomoSE <jonathancggs@gmail.com>
2021-09-10 09:59:55 -0500

    🧩Entity
    Adding enum entity for Dificulty

---
+++ src/main/java/com/devlomose/recipe/domain/Difficulty.java
@@ -0,0 +1,11 @@
+package com.devlomose.recipe.domain;
+
+/**
+ * Difficulty at: src/main/java/com/devlomose/recipe/domain
+ * Created by @DevLomoSE at 10/9/21 9:48.
+ */
+public enum Difficulty {
+    EASY,
+    MODERATE,
+    HARD
+}
